b6879d49307c282d7a40e4c4e1e4b6855ef2c5fd
mariusz <devgluszak@gmail.com>
2022-01-17 01:04:36 +0100

    header(4) large.scss - add @media(min-width:1200px) change font-size:2.2rem, main.js - add variables navMobile, navBtn, footerYear, navMobLinks, functions: handleCurentYear, handleNav, add actionListener on navBtn

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log('hej');
+const navMobile = document.querySelector('.nav-mobile')
+const navBtn = document.querySelector('.hamburger')
+const footerYear = document.querySelector('.footer__year')
+const navMobLinks = document.querySelectorAll('.nav-mobile > .nav__link')
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+handleCurrentYear()
+
+const handleNav = () => {
+	navBtn.classList.toggle('is-active')
+	navMobile.classList.toggle('nav-mobile--active')
+
+	navMobLinks.forEach(nav => {
+		nav.addEventListener('click', () => {
+			navMobile.classList.remove('nav-mobile--active')
+		})
+	})
+}
+
+navBtn.addEventListener('click', handleNav)
